ea3fb395f6e49fdf4a65d84b724f32a89a8d9438
Hemant Kushwaha <hk575575575@gmail.com>
2025-08-07 10:19:03 +0530

    Make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react';
+
 import './App.css';
-import SearchBox from './SearchBox';
-import InfoBox from './infoBox';
+
 import WeatherApp from './WeatherApp';
 
 function App() {
-  
+
 
   return (
     <>
-    <WeatherApp/>
+      <WeatherApp />
     </>
   )
 }
