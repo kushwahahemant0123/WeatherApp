bdc01c9bd68d873fa68ac26887196f7575b0aac5
Hemant Kushwaha <hk575575575@gmail.com>
2025-08-07 10:07:39 +0530

    Make it responsive

--- src/WeatherApp.jsx
+++ src/WeatherApp.jsx
@@ -1,26 +1,27 @@
 import InfoBox from "./infoBox";
 import SearchBox from "./SearchBox";
 import { useState } from 'react';
+import "./WeatherApp.css"
 
-export default function WeatherApp(){
-    const [weatherInfo, setWeatherInfo]=useState({
-        city :"Begamganj",
+export default function WeatherApp() {
+    const [weatherInfo, setWeatherInfo] = useState({
+        city: "Begamganj",
         temp: 25.05,
-            tempMin: 25.05,
-            rempMax:25.05,
-            humidity:47,
-            feelsLike:24.84,
-            weather: "haze",
+        tempMin: 25.05,
+        rempMax: 25.05,
+        humidity: 47,
+        feelsLike: 24.84,
+        weather: "haze",
     });
-    let updateInfo=(result)=>{
+    let updateInfo = (result) => {
         setWeatherInfo(result);
     }
 
-    return(
-        <div style={{textAlign:"center"}}>
+    return (
+        <div style={{ textAlign: "center" }}>
             <h2>Weather App By Hemant Kushwha (Avi)</h2>
-            <SearchBox updateInfo={updateInfo}/>
-            <InfoBox info={weatherInfo}/>
+            <SearchBox updateInfo={updateInfo} />
+            <InfoBox info={weatherInfo} />
         </div>
     )
 }
